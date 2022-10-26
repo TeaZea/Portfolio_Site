a256f6e9cf5e589e3ef30560f4a37099d6a7c390
Stefano <sena.stefano@gmail.com>
2022-10-26 14:54:24 -0400

    Added script to copy my email to the clipboard

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// Typewriter effect
 var i = 0;
 var txt = 'Hello! My name is Stef!'; /* The text */
 var speed = 150; /* The speed/duration of the effect in milliseconds */
@@ -10,8 +11,21 @@ function typeWriter() {
   }
 }
 
+
+// Blinking cursor effect
 let blink_speed = 1000;
 let t = setInterval(function () {
     let ele = document.getElementById('myBlinkingDiv');
     ele.style.visibility = (ele.style.visibility == 'hidden' ? '' : 'hidden');
 }, blink_speed);
+
+
+function myEmail() {
+  const email = "sena.stefano@gmail.com";
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(email);
+  
+  // Alert
+  alert("Copied Stef's email: " + email);
+};
